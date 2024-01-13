cc7a44fe8505dbdae58506ddc69d619c1a95b73e
subhash131 <subhashnayak131@gmail.com>
2024-01-13 22:54:04 +0530

    Hero section

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,15 +1,18 @@
 import React from "react";
-import TextAnimate from "../designs/TextAnimate";
+
+const StyledText = ({ children }: { children: React.ReactNode }) => {
+  return <span className="text-[var(--text)] inline-block">{children}</span>;
+};
 
 const Hero = () => {
   return (
     <div className=" absolute w-full h-[calc(100vh-8rem)] bg-red-5 justify-center items-center flex bg-re-50">
       <div className="bg-gree-300 w-[80%] ">
         <p className="text-white text-7xl uppercase font-bold leading-tight">
-          Crafting digital <TextAnimate>solutions</TextAnimate> through{" "}
-          <TextAnimate>code</TextAnimate>, a software
-          <TextAnimate>engineer</TextAnimate> transforms complexity into
-          seamless <TextAnimate>functionality</TextAnimate>
+          Crafting digital <StyledText>solutions</StyledText> through{" "}
+          <StyledText>code</StyledText>, a software
+          <StyledText>engineer</StyledText> transforms complexity into seamless{" "}
+          <StyledText>functionality</StyledText>
         </p>
       </div>
     </div>
