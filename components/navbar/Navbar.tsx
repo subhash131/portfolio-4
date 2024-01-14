91867fbfc3ad842671ae61d8aaa26604f61d9105
subhash131 <subhashnayak131@gmail.com>
2024-01-14 11:05:44 +0530

    update navbar styles

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import Glow from "../designs/Glow";
 
 const Navbar = () => {
   return (
-    <div className="w-full flex items-center justify-center h-32 sticky top-0">
+    <div className="w-full flex items-center justify-center h-32 sticky -top-6 z-30">
       <Glow className="w-96 shadow-cus-wide-3xl absolute" />
       <Logo />
     </div>
